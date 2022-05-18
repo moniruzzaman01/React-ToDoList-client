aba213be1c8d3d3d4a971207c2085c1792558a5f
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-05-18 21:29:54 +0600

    firebase config added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,9 +4,10 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div>
-      <h2 className=" text-3xl lg:text-4xl text-center mb-10 text-orange-400 font-bold">
+      <h2 className=" text-3xl lg:text-4xl text-center my-2 text-orange-400 font-bold">
         Welcome to To Do List !!!
       </h2>
+      <p className="text-xl text-white mb-10">Please Login/Signup to use.</p>
       <Link to="login" className="btn btn-outline btn-primary mr-5 ">
         Login
       </Link>
