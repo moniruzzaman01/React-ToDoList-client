8ceb247022a67d69dfbc4b372b03f7a10e1f80bb
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-05-18 17:55:15 +0600

    Todo form added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import "./App.css";
+import ToDoList from "./Components/Todolist/ToDoList";
 
 function App() {
   return (
-    <div className="App">
-      <button class="btn btn-primary">Button</button>
+    <div className=" flex justify-center items-center h-screen">
+      <div className=" max-w-md w-full">
+        <ToDoList />
+      </div>
     </div>
   );
 }
--- src/Components/Todolist/ToDoList.js
+++ src/Components/Todolist/ToDoList.js
@@ -2,8 +2,26 @@ import React from "react";
 
 const ToDoList = () => {
   return (
-    <div>
-      <h2>ToDoList</h2>
+    <div className=" lg:max-w-lg md:max-w-md mx-auto my-20 px-5">
+      <h2 className=" text-2xl text-white font-bold">To Do List</h2>
+      <form>
+        <br />
+        <input
+          type="text"
+          name="email"
+          placeholder="Task Name"
+          className="input mb-5 input-bordered w-full max-w-lg"
+        />
+        <br />
+        <textarea
+          class="textarea textarea-bordered mb-5 w-full max-w-lg"
+          placeholder="Task Description"
+        ></textarea>
+        <br />
+        <button className="btn btn-accent w-full text-white mt-2">
+          add task
+        </button>
+      </form>
     </div>
   );
 };
